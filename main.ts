bb8158583e8b63e03c11c648cb51e5505aef6619
Ophelie Bugnon <ophelie.bugnon@edu.hefr.ch>
2025-01-23 17:44:11 +0100

    [FEAT]: Added dockerfile and deno config

--- main.ts
+++ main.ts
@@ -1,5 +1,8 @@
-function greet(name: string): string {
-  return `Hello, ${name}!`;
-}
+import { serve } from "https://deno.land/std@0.218.2/http/server.ts";
 
-console.log(greet("world"));
+const handler = (req: Request): Response => {
+  return new Response("Hello from Deno!", { status: 200 });
+};
+
+console.log("Server running on http://localhost:8000");
+serve(handler, { port: 8000 });
