c39c6dade015dd48ceb49fcda0bbeffbc09551e5
Ophelie Bugnon <ophelie.bugnon@edu.hefr.ch>
2025-01-25 11:26:27 +0100

    [FEAT]: Created fresh project / Dockerfile and docker-compose

--- main.ts
+++
@@ -1,8 +0,0 @@
-import { serve } from "https://deno.land/std@0.218.2/http/server.ts";
-
-const handler = (req: Request): Response => {
-  return new Response("Hello from Deno!", { status: 200 });
-};
-
-console.log("Server running on http://localhost:8000");
-serve(handler, { port: 8000 });
